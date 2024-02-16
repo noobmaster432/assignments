122e01b9490059346d02768b93d58e088ae25ed5
Gyanendra Tiwari <mrgyan432@gmail.com>
2024-02-16 20:43:37 +0530

    week 6 assignment done

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
@@ -10,12 +10,27 @@ export const Assignment3 = () => {
         { name: 'Tomato', value: 30 },
         // Add more items as needed
     ]);
+    const [item, setItem] = useState('');
+    const [itemVal, setItemVal] = useState(0);
+
+    const addItem = () => {
+        setItems([...items, { name: item, value: Number(itemVal) }]);
+        setItem('');
+        setItemVal(0);
+    }
 
     // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
+    const totalValue = useMemo(() => {
+        return items.reduce((acc, item) => acc + item.value, 0);
+    }, [items]);
+
     return (
         <div>
+            <div>
+                <input type="text" value={item} onChange={e => setItem(e.target.value)} /><br/><br/>
+                <input type="number" value={itemVal} onChange={e => setItemVal(e.target.value)} /><br/><br/>
+                <button onClick={addItem}>Add Item</button>
+            </div>
             <ul>
                 {items.map((item, index) => (
                     <li key={index}>{item.name} - Price: ${item.value}</li>
